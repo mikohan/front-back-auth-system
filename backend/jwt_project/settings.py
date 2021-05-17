f3c0460a4ac253440547427b33e3595e3e8d9298
mikohan <angara99@gmail.com>
2021-05-17 09:14:55 +0300

    Sign in starts

--- backend/jwt_project/settings.py
+++ backend/jwt_project/settings.py
@@ -10,8 +10,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-6eb2hoj^vk@-!k3(^5pbkw$lntx^p%53+kjkc$du++=**@@y7z"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
