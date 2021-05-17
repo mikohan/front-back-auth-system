598b35b712ab3490bdcd5e04824844103a3e2fd1
mikohan <angara99@gmail.com>
2021-05-17 09:26:47 +0300

    Reducer done

--- frontend/src/store/users/userReducer.ts
+++ frontend/src/store/users/userReducer.ts
@@ -10,6 +10,10 @@ import {
   USER_PASSWORD_RESET_FAIL,
   USER_PASSWORD_RESET_CONFIRM_SUCCESS,
   USER_PASSWORD_RESET_CONFIRM_FAIL,
+  USER_SIGN_UP_SUCCESS,
+  USER_SIGN_UP_FAIL,
+  USER_ACTIVATION_SUCCESS,
+  USER_ACTIVATION_FAIL,
   IUserAction,
 } from './userActionTypes';
 import { IUserState } from '../../intefaces';
@@ -45,16 +49,6 @@ export const userReducer = (
         access: action.payload?.access,
         refresh: action.payload?.refresh,
       };
-    case USER_LOGIN_FAIL:
-      localStorage.removeItem('access');
-      localStorage.removeItem('refresh');
-      return {
-        ...state,
-        isAuthenticated: false,
-        access: null,
-        refresh: null,
-        user: null,
-      };
     case USER_LOADED_SUCCESS:
       return {
         ...state,
@@ -66,6 +60,8 @@ export const userReducer = (
         user: null,
       };
     case USER_LOGOUT:
+    case USER_LOGIN_FAIL:
+    case USER_SIGN_UP_FAIL:
       localStorage.removeItem('access');
       localStorage.removeItem('refresh');
       return {
@@ -79,6 +75,8 @@ export const userReducer = (
     case USER_PASSWORD_RESET_FAIL:
     case USER_PASSWORD_RESET_CONFIRM_SUCCESS:
     case USER_PASSWORD_RESET_CONFIRM_FAIL:
+    case USER_ACTIVATION_SUCCESS:
+    case USER_ACTIVATION_FAIL:
       return {
         ...state,
       };
