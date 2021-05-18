24a0d5d82fffef93278d4c28de30f7f71bdf70f9
mikohan <angara99@gmail.com>
2021-05-18 09:57:50 +0300

    Vk start cteate

--- backend/jwt_project/urls.py
+++ backend/jwt_project/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
     path("auth/", include("djoser.social.urls")),
+    path("auth/", include("rest_framework_social_oauth2.urls")),
 ]
 
 urlpatterns += [re_path(r"^.*", TemplateView.as_view(template_name="index.html"))]
